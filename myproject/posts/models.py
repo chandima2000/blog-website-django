1f4c986ae3b84cda33be6224260461d3e8604e0f
chandima2000 <hychandima2000@gmail.com>
2024-05-26 23:48:46 +0530

    Create a model called Post

--- myproject/posts/models.py
+++ myproject/posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+# In Django, data is created in objects, called Models, 
+# A Django model is a table in your database.
+
+class Post(models.Model):
+    title = models.CharField(max_length=75)
+    body = models.TextField()
+    slug = models.SlugField()
+    date = models.DateTimeField(auto_now_add=True)
