616cd35f79c17c8ee5c45cd0ce1946e4c550e04b
chandima2000 <hychandima2000@gmail.com>
2024-05-27 22:03:09 +0530

    Make Changes to Deploy

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import os  # Add os module.
+# import os  # Add os module.
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-db$5ed_2z&h6&u09oqb)2)sf2^zgd&24+!3da$!i5w!y9uw#f*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost","127.0.0.1"]
 
 
 # Application definition
@@ -118,17 +118,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
 
+STATIC_ROOT = BASE_DIR / 'assets'
+MEDIA_ROOT = BASE_DIR / 'media'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static') 
+    BASE_DIR / 'static'
      ## This tells to the DJango, where the "static" folder is located.
 ]
 
-
-## Upload Images
-MEDIA_URL = 'media/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
