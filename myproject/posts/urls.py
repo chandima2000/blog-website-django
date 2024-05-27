71ceddee0979ee71d464b611a0e38e8ee29e220e
chandima2000 <hychandima2000@gmail.com>
2024-05-27 21:09:23 +0530

    create Authorization functionalities

--- myproject/posts/urls.py
+++ myproject/posts/urls.py
@@ -8,6 +8,7 @@ app_name = 'posts'
 urlpatterns = [
 
     path('',views.posts_list,name="list"),   #add a path to the home page
+    path('new-post/',views.post_new, name="new-post"),
     path('<slug:slug>',views.post_page,name="page"),
 
 
