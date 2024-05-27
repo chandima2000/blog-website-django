96c719c64ff69de560d32d4af55df66f5ce6cb15
chandima2000 <hychandima2000@gmail.com>
2024-05-27 12:56:53 +0530

    Create URLs and Slug for each post

--- myproject/posts/urls.py
+++ myproject/posts/urls.py
@@ -3,9 +3,12 @@ from django.urls import path
 # Import views
 from . import views
 
+app_name = 'posts'
 
 urlpatterns = [
 
-    path('',views.posts_list),   #add a path to the home page
+    path('',views.posts_list,name="list"),   #add a path to the home page
+    path('<slug:slug>',views.post_page,name="page"),
+
 
 ]
